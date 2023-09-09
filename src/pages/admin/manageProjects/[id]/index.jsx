3304e4ab2817aa1661494a2ac0d542ca9f685785
Muhammad Faizan <muhammadfaizan027915@gmail.com>
2023-09-09 10:09:34 +0500

    Added charts in project page.

--- src/pages/admin/manageProjects/[id]/index.jsx
+++ src/pages/admin/manageProjects/[id]/index.jsx
@@ -4,8 +4,12 @@ import Button from "../../../../common/button";
 import Container from "../../../../common/container";
 import DateInput from "../../../../common/dateInput";
 import { useDimensions } from "../../../../hooks";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 
-const dateInputClasses = `!font-semibold !text-white !bg-[#34F5C5] border-none focus-within:!outline-none white-placeholder h-10 w-40`;
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const dateInputClasses = `!font-semibold !text-white !bg-[#34F5C5] border-none focus-within:!outline-none white-placeholder h-10 w-[160px]`;
 
 function Project() {
   const dimension = useDimensions();
@@ -45,36 +49,55 @@ function Project() {
 
         <div className="py-4">
           <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
-              <p className="">
-                <span className="font-bold mr-3">Project Name: </span>
-                <span className="text-[#009379]">Develop Sun's prject</span>
-              </p>
+            <p className="">
+              <span className="font-bold mr-3">Project Name: </span>
+              <span className="text-[#34F5C5]">Develop Sun's prject</span>
+            </p>
 
-              <p className="">
-                <span className="font-bold mr-3">Start Date: </span>
-                <span className="text-[#009379]">Mon, Jan 4, 2012</span>
-              </p>
+            <p className="">
+              <span className="font-bold mr-3">Start Date: </span>
+              <span className="text-[#34F5C5]">Mon, Jan 4, 2012</span>
+            </p>
 
-              <p className="">
-                <span className="font-bold mr-3">End Date: </span>
-                <span className="text-[#009379]">Mon, Jan 4, 2012</span>
-              </p>
+            <p className="">
+              <span className="font-bold mr-3">End Date: </span>
+              <span className="text-[#34F5C5]">Mon, Jan 4, 2012</span>
+            </p>
 
-              <p className="">
-                <span className="font-bold mr-3">Assigned Workers: </span>
-                <span className="text-[#009379]">3</span>
-              </p>
+            <p className="">
+              <span className="font-bold mr-3">Assigned Workers: </span>
+              <span className="text-[#34F5C5]">3</span>
+            </p>
 
-              <p className="">
-                <span className="font-bold mr-3">Supervisor: </span>
-                <span className="text-[#009379]">Muhammad Faizan</span>
-              </p>
+            <p className="">
+              <span className="font-bold mr-3">Supervisor: </span>
+              <span className="text-[#34F5C5]">Muhammad Faizan</span>
+            </p>
 
-              <p className="">
-                <span className="font-bold mr-3">Workers: </span>
-                <span className="text-[#009379]">Muhammad Faizan, Sudais Malik, Ali Abdullah</span>
-              </p>
+            <p className="">
+              <span className="font-bold mr-3">Workers: </span>
+              <span className="text-[#34F5C5]">
+                Muhammad Faizan, Sudais Malik, Ali Abdullah
+              </span>
+            </p>
           </div>
+        </div>
+
+        <div className="w-40 h-40">
+          <Doughnut
+            title="Data"
+            data={{
+              datasets: [
+                {
+                  label: "# of Numbers",
+                  data: [36, 12, 5],
+                  weight: 2,
+                  backgroundColor: ["red", "yellow", "blue"],
+                },
+              ],
+              labels: ["Completed", "Remaining", "Inprogres"],
+            }}
+          />
         </div>
       </Container>
     </Layout>
