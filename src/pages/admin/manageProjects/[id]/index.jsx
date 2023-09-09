91176835b3790f5bcae325ec9876e27c5e92a6c2
Muhammad Faizan <muhammadfaizan027915@gmail.com>
2023-09-09 19:43:08 +0500

    Added charts in project item page.

--- src/pages/admin/manageProjects/[id]/index.jsx
+++ src/pages/admin/manageProjects/[id]/index.jsx
@@ -4,12 +4,29 @@ import Button from "../../../../common/button";
 import Container from "../../../../common/container";
 import DateInput from "../../../../common/dateInput";
 import { useDimensions } from "../../../../hooks";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Tooltip,
+  Legend,
+  Title,
+} from "chart.js";
+import { Doughnut, Bar } from "react-chartjs-2";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+  BarElement,
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+  Title
+);
 
-const dateInputClasses = `!font-semibold !text-white !bg-[#34F5C5] border-none focus-within:!outline-none white-placeholder h-10 w-[160px]`;
+const dateInputClasses = `!font-semibold !text-white !bg-[#34F5C5] border-none focus-within:!outline-none white-placeholder h-10 !w-40`;
 
 function Project() {
   const dimension = useDimensions();
@@ -83,21 +100,133 @@ function Project() {
           </div>
         </div>
 
-        <div className="w-40 h-40">
-          <Doughnut
-            title="Data"
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 sm:gap-4 items-center">
+          <div>
+            <p className="mb-2">
+              <span className="inline-block w-10/12 md:w-60">
+                Total Surveys Completed
+              </span>
+              <span className="text-[#00ABE0] before:content-[':'] before:mr-4">
+                35
+              </span>
+            </p>
+            <p className="mb-2">
+              <span className="inline-block w-10/12 md:w-60">
+                Surveys Remaining
+              </span>
+              <span className="text[#FF7258] before:content-[':'] before:mr-4">
+                12
+              </span>
+            </p>
+
+            <p className="mb-2">
+              <span className="inline-block w-10/12 md:w-60">
+                Surverys in Progress
+              </span>
+              <span className="text-[#FFC94A] before:content-[':'] before:mr-4">
+                35
+              </span>
+            </p>
+          </div>
+          <div className="w-40 h-40 md:w-60 md:h-60 mx-auto">
+            <Doughnut
+              title="Data"
+              data={{
+                datasets: [
+                  {
+                    label: "# of tasks",
+                    data: [36, 12, 5],
+                    weight: 2,
+                    clip: 4,
+                    backgroundColor: ["#00ABE0", "#FF7258", "#FFC94A"],
+                  },
+                ],
+              }}
+              options={{
+                responsive: true,
+                maintainAspectRatio: true,
+                cutout: "70%",
+              }}
+            />
+          </div>
+
+          <div>
+            <p className="flex items-center mb-2">
+              <span className="w-10/12 md:w-60 line-clamp-1">
+                Surveys Completed by <span className="text-[#34F5C5]">Muhammad Faizan</span>
+              </span>
+              <span className="before:content-[':'] before:mr-4">35</span>
+            </p>
+
+            <p className="flex items-center mb-2">
+              <span className="w-10/12 md:w-60 line-clamp-1">
+                Surveys Completed by <span className="text-[#34F5C5]">Ali Abdullah</span>
+              </span>
+              <span className="before:content-[':'] before:mr-4">90</span>
+            </p>
+
+            <p className="flex items-center mb-2">
+              <span className="w-10/12 md:w-60 line-clamp-1">
+                Surveys Completed by <span className="text-[#34F5C5]">Ahmer Shahid</span>
+              </span>
+              <span className="before:content-[':'] before:mr-4">120</span>
+            </p>
+          </div>
+        </div>
+
+        <div className="mt-8 md:w-8/12 max-w-[700px] mx-auto">
+          <Bar
+            options={{
+              responsive: true,
+              maintainAspectRatio: true,
+              plugins: {
+                title: { text: "Number of hours worked" },
+              },
+            }}
             data={{
+              labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
               datasets: [
                 {
-                  label: "# of Numbers",
-                  data: [36, 12, 5],
-                  weight: 2,
-                  backgroundColor: ["red", "yellow", "blue"],
+                  label: "Muhammad Faizan",
+                  data: [3, 4, 6, 8, 2],
+                  backgroundColor: "#23F08A",
+                },
+                {
+                  label: "Ahmer Shahid",
+                  data: [6, 2, 9, 1, 0],
+                  backgroundColor: "#5D3E8E",
+                },
+                {
+                  label: "Ali Abdullah",
+                  data: [2, 8, 1, 9, 2],
+                  backgroundColor: "#686D74",
                 },
               ],
-              labels: ["Completed", "Remaining", "Inprogres"],
             }}
           />
+
+          <div className="mx-auto sm:w-fit">
+            <p className="flex items-center mb-2">
+              <span className="w-9/12 md:w-96 line-clamp-1">
+                Surveys Completed by <span className="text-[#34F5C5]">Muhammad Faizan</span>
+              </span>
+              <span className="before:content-[':'] before:mr-4">35 hrs</span>
+            </p>
+
+            <p className="flex items-center mb-2">
+              <span className="w-9/12 md:w-96 line-clamp-1">
+                Surveys Completed by <span className="text-[#34F5C5]">Ali Abdullah</span>
+              </span>
+              <span className="before:content-[':'] before:mr-4">90 hrs</span>
+            </p>
+
+            <p className="flex items-center mb-2">
+              <span className="w-9/12 md:w-96 line-clamp-1">
+                Surveys Completed by <span className="text-[#34F5C5]">Ahmer Shahid</span>
+              </span>
+              <span className="before:content-[':'] before:mr-4">120 hrs</span>
+            </p>
+          </div>
         </div>
       </Container>
     </Layout>
