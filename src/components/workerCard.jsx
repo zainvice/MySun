13783317cd43de4fc7f6b726c64d6c174ee7d284
zainvice <amazwings@gmail.com>
2023-09-26 18:49:32 +0500

    Dashboard Integrated!

--- src/components/workerCard.jsx
+++ src/components/workerCard.jsx
@@ -1,35 +1,77 @@
 import { publicUrl } from "../utils";
 import { useTranslation } from 'react-i18next';
 
-function WorkerCard() {
+function formatTimeDifference(lastLogin) {
+  const now = new Date();
+  const timeDifference = now - lastLogin;
+
+  const minutesDifference = Math.floor(timeDifference / (1000 * 60));
+
+  if (minutesDifference < 1) {
+    return "Just now";
+  } else if (minutesDifference === 1) {
+    return "a minute ago";
+  } else if (minutesDifference < 60) {
+    return `${minutesDifference} minutes ago`;
+  } else {
+    
+    const hoursDifference = Math.floor(minutesDifference / 60);
+    
+    if (hoursDifference === 1) {
+      return "an hour ago";
+    } else {
+      return `${hoursDifference} hours ago`;
+    }
+  }
+}
+
+function WorkerCard(workers) {
+ 
+  const worker= workers?.worker
   const { t } = useTranslation();
+  const lastLogin = new Date(worker?.lastLogin)
+  const formattedTimeDifference = formatTimeDifference(lastLogin);
+  //const startedOn = new Date(worker?.createdAt)
   
   return (
-    <div className="w-full p-4 sm:p-6 rounded-3xl text-white shadow-lg bg-gradient-to-b from-[#00FFD3] from-60% to-[#37C19F]">
+    <>
+    {worker ? (
+      <>
+       <div className="w-full p-4 sm:p-6 rounded-3xl text-white shadow-lg bg-gradient-to-b from-[#00FFD3] from-60% to-[#37C19F]">
       <div className="flex items-center gap-4">
         <img src={`${publicUrl}/images/avatarMale.png`} />
         <div>
           <h2 className="font-semibold text-2xl line-clamp-1">
-            WORKER NAME
+            {worker?.fullName}
           </h2>
           <p className="text-sm font-medium flex items-center gap-2">
             <span className="font-semibold">{t('workerCard.lastLoginLabel')}: </span>{" "}
-            <span>TIMESTAMP</span>
+            <span>{formattedTimeDifference}</span>
           </p>
         </div>
       </div>
       <div>
-        <p className="mt-2">75 {t('workerCard.projectsCompleted')}</p>
+     
+      <p className="mt-2">{worker?.completedTasksCount} {t('workerCard.projectsCompleted')}</p>
+
         <p className="mt-2 flex items-center gap-2 text-sm">
           <span className="font-semibold">{t('workerCard.startedOnLabel')}: </span>
-          <span>TIMESTAMP</span>
+          <span>{worker?.createdAt && new Date(worker.createdAt).toLocaleString()}</span>
+
         </p>
         <p className="mt-2 flex items-center gap-2 text-sm">
           <span className="font-semibold">{t('workerCard.latestProjectLabel')}: </span>
-          <span>ASSIGNED PROJECT</span>
+          <span>{worker?.assignedProject?.projectName}</span>
         </p>
       </div>
     </div>
+      </>
+    ): (
+      <></>
+    )
+    }
+   
+    </>
   );
 }
 
--- src/pages/admin/dashboard.jsx
+++ src/pages/admin/dashboard.jsx
@@ -5,11 +5,106 @@ import ProjectCard, { VARIANTS } from "../../components/projectCard";
 import WorkerCard from "../../components/workerCard";
 import WorkerDetail from "../worker/workerDetail";
 import { useTranslation } from "react-i18next";
-
+import { useEffect, useState } from "react";
+import { getProjects } from "../../api";
+import Spinner from "../../common/spinner";
+import { useProjects } from "../../context/projectsContext";
 import Layout from "../../layout";
+import { getWorkers } from "../../api";
 import { NavLink } from "react-router-dom";
+function findProjectByWorker(worker, projects) {
+  
+  const project = projects.find(project => project._id === worker.projects[0]);
+  
+  return project;
+}
+
 function Dashboard() {
   const { t } = useTranslation();
+  const { projects, setProjects } = useProjects();
+  const [workers, setWorkers] = useState([]);
+  const [isloading, setloading] = useState(true);
+  const [project, toDisplay]= useState();
+  const [workerNo1, setWorkerNo1] = useState(null);
+  const [workerNo2, setWorkerNo2] = useState(null);
+  
+  useEffect(() => {
+    if (projects) {
+      getWorkers()
+        .then((data) => {
+   
+          const workerList = data.filter((worker) => worker.role === "worker");
+        
+         
+          workerList.forEach((worker) => {
+            const completedTasks = worker.tasks.filter((task) => {
+              // I'll define completed crieteria here
+              //tasksCompleted++
+            });
+            worker.completedTasksCount = completedTasks.length;
+          });
+        
+          
+          const sortedWorkers = workerList.sort(
+            (a, b) => b.completedTasksCount - a.completedTasksCount
+          );
+          
+          
+          const topTwoWorkers = sortedWorkers.slice(0, 2);
+
+          if (topTwoWorkers.length > 0) {
+            setWorkerNo1(topTwoWorkers[0]);
+          }
+          if (topTwoWorkers.length > 1) {
+            setWorkerNo2(topTwoWorkers[1]);
+          }
+          if (topTwoWorkers.length > 0) {
+            setWorkerNo1(topTwoWorkers[0]);
+            const assignedProject1 = findProjectByWorker(workerNo1, projects);
+           
+            setWorkerNo1(prevWorkerNo1 => ({ ...prevWorkerNo1, assignedProject: assignedProject1 }));
+          }
+          if (topTwoWorkers.length > 1) {
+            setWorkerNo2(topTwoWorkers[1]);
+            const assignedProject2 = findProjectByWorker(workerNo2, projects);
+            
+            setWorkerNo2(prevWorkerNo2 => ({ ...prevWorkerNo2, assignedProject: assignedProject2 }));
+          }
+          
+          workerNo1.assignedProject = findProjectByWorker(workerNo1, projects)
+          setWorkers(data);
+          setloading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching workers:", error);
+        });
+    }
+  }, [projects]);
+
+
+  useEffect(() => {     
+    getProjects()
+      .then((data) => {
+        setProjects(data);
+        
+  
+        // Sort the projects by the 'updatedOn' field in descending order
+        const sortedProjects = data.sort((a, b) => {
+          const dateA = new Date(a.updatedOn);
+          const dateB = new Date(b.updatedOn);
+          return dateB - dateA;
+        });
+  
+        // Get the latest updated project (first item in the sorted array)
+        const latestUpdatedProject = sortedProjects[0];
+        toDisplay(latestUpdatedProject)
+        // Display the latest updated project
+        console.log("Latest Updated Project:", latestUpdatedProject);
+      })
+      .catch(() => localStorage.removeItem("projects"))
+      .finally(() => setloading(false));
+  }, []);
+
   return (
     <Layout activePageName={t("dashboard.title")}>
       <Container>
@@ -47,7 +142,12 @@ function Dashboard() {
         <div className="flex flex-col sm:flex-row gap-4 sm:gap-6">
           <div className="sm:w-1/2 md:min-w-[340px] md:w-1/3 ">
             <NavLink to="/manage-projects/:id" className={'block text-white transform transition-transform hover:scale-105'}>
-              <ProjectCard variant={VARIANTS.GREEN} />
+              <ProjectCard
+                    project={project}
+                    variant={
+                       VARIANTS.GREEN
+                    }
+                  />
             </NavLink>
           </div>
           <div className="flex-1 bg-gradient-to-b from-[#00FFD3] from-60% to-[#37C19F] rounded-3xl p-[3px] shadow-lg">
@@ -95,17 +195,22 @@ function Dashboard() {
         />
         <div className="flex flex-col lg:flex-row gap-4 sm:gap-6">
           <div className="lg:w-3/5 grid sm:grid-cols-2 gap-4">
-            {Array(2)
-              .fill(0)
-              .map((_, index) => (
+            
                 <NavLink
                   to="/worker-detail"
-                  key={index}
+                  
                   className="block transform hover:scale-105 transition-transform duration-300"
                 >
-                  <WorkerCard />
+                  <WorkerCard worker= {workerNo1} />
                 </NavLink>
-              ))}
+                <NavLink
+                  to="/worker-detail"
+                  
+                  className="block transform hover:scale-105 transition-transform duration-300"
+                >
+                  <WorkerCard worker= {workerNo2} />
+                </NavLink>
+            
           </div>
           <div className="flex-1"></div>
         </div>
